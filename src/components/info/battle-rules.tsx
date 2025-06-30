e34e07d6aaed1bc225b8ef2324772dde9c5b06c0
Gabriel Duarte <jds.gabrielduarte@gmail.com>
2025-06-30 19:38:22 -0300

    fix: correct spacing in battle rules description for improved readability

--- src/components/info/battle-rules.tsx
+++ src/components/info/battle-rules.tsx
@@ -12,7 +12,7 @@ export function BattleRules() {
         </p>
 
         <ul className="list-disc list-inside text-white space-y-2 text-base">
-          <li>Cada monstro possui<span className="text-yellow-400 font-bold">2 itens</span> dentro da arena:</li>
+          <li>Cada monstro possui <span className="text-yellow-400 font-bold">2 itens</span> dentro da arena:</li>
           <ul className="ml-6 list-none">
             <li><span className="text-red-400 font-bold inline-block">Ataque forte <Sword className="size-4 inline-block" /></span> - Dobra o valor do ataque normal, pode ser usado 1 vez.</li>
             <li><span className="text-blue-400 font-bold inline-block">Defesa <Shield className="size-4 text-blue-400 inline-block" /></span> -  Protege de um ataque, pode ser usada 2 vezes.</li>
